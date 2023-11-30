3952a8ebd7cc053ad4198fb6d9438d5c79ce1a9f
Kate.Zhang <Kate.Zhang@noaa.gov>
2023-11-30 23:49:39 +0000

    Fire blending scripts for GBBPEx v004 data May need minor modifications when apply to other fire emission products

---
+++ job_ave_gbbepx.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+module use -a /contrib/anaconda/modulefiles
+module load anaconda/latest
+module load nco
+
+gbbdir=/scratch1/NCEPDEV/rstprod/nexus_emissions/GBBEPx
+#outdir=/scratch1/NCEPDEV/rstprod/nexus_emissions/GBBEPx/blending
+outdir=/scratch1/BMC/gsd-fv3-dev/lzhang/blend/test/
+vars="BC CH4 CO CO2 MeanFRP NH3 NOx OC PM2.5 SO2"
+echo ${vars} > invar.nml
+
+yy=2016
+mm=08
+dd=01
+
+emidate=$yy$mm$dd
+
+
+istart=1 # istart can be flexible ajdusted to any day (such as 6 to keep the first 6 the same)
+iday=1 #iday starts from istart to iend
+iend=35
+
+        j_day=$(date -d "$emidate" "+%j")
+        echo "Julian day: $j_day"
+        end_day=$((j_day+iend-1))
+        echo "end_day: $end_day"
+
+         end_day_of_year=$end_day
+         end_date=$(date -d "$yy-01-01 +$(( $end_day_of_year - 1 )) days" +"%Y-%m-%d")
+         echo "Date: $end_date"
+         end_mm=$(echo $end_date | awk -F'-' '{print $2}')
+         end_dd=$(echo $end_date | awk -F'-' '{print $3}')
+
+#cp  ${gbbdir}/climMean/GBBEPx-all01GRID_v4r0_climMean_${end_mm}${end_dd}.nc climate.nc
+#      echo "cp climMean/GBBEPx-all01GRID_v4r0_climMean_${end_mm}${end_dd}.nc climate.nc"
+       for (( i=${j_day}; i<=${end_day}; i++ ))
+       do
+         jd=$((i-j_day+1))
+           iday=${jd}
+         day_of_year=$i
+         date=$(date -d "$yy-01-01 +$(( $day_of_year - 1 )) days" +"%Y-%m-%d")
+         echo "Date: $date"
+         nmonth=$(echo $date | awk -F'-' '{print $2}')
+         nday=$(echo $date | awk -F'-' '{print $3}')
+
+cp  ${gbbdir}/climMean/GBBEPx-all01GRID_v4r0_climMean_${nmonth}${nday}.nc climate.nc
+      echo "cp climMean/GBBEPx-all01GRID_v4r0_climMean_${nmonth}${nday}.nc climate.nc"
+
+cp ${gbbdir}/GBBEPx_all01GRID.emissions_v004_${yy}${mm}${dd}.nc input.nc
+
+   echo "cp GBBEPx_all01GRID.emissions_v004_${yy}${mm}${dd}.nc input.nc "
+   echo "iday: ${iday}"  
+
+#if [ $iday -ge  $istart ]; then
+python ave_gbbepx.py  -v invar.nml $iday $istart $iend 
+
+#fi
+
+ncatted -O -a units,time,o,c,"hours since ${yy}-${nmonth}-${nday} 12:00:00" input.nc
+
+mv input.nc $outdir/GBBEPx_all01GRID.emissions_v004_${yy}${nmonth}${nday}.nc
+
+       done
+       
---
+++ linear_gbbepx.py
@@ -0,0 +1,59 @@
+import datetime as dt
+import netCDF4 as nc
+import os, argparse
+import numpy as np
+
+'''
+Command:
+    python test_gbbepx.py -v INVARS.nml 
+'''
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=(
+            'Linear interpolation applied to real-time and climate GBBEPx emission for S2S'
+        )
+    )
+
+
+    required = parser.add_argument_group(title='required arguments')
+
+    required.add_argument(
+        '-v', '--variable',
+        help="Input file containing the  variables",
+        type=str, required=True)
+
+    parser.add_argument("iday_new")
+    parser.add_argument("istart_new")
+    parser.add_argument("iend_new")
+
+
+
+    args = parser.parse_args()
+    invarnml = args.variable
+
+    iday = int(args.iday_new)
+    istart = int(args.istart_new)
+    iend = int(args.iend_new)
+
+    #print("iday=", args.iday)
+    #print("istart=", args.istart)
+    #print("iend=", args.iend)
+
+    infile1='input.nc'
+    infile2='climate.nc'
+
+    with open(invarnml, 'r') as fd:
+	    invars=fd.read().strip().split(' ')
+    print(invars)
+
+#    import sys
+#    sys.exit("stop")
+
+    with nc.Dataset(infile1,'a') as infile1:
+        with nc.Dataset(infile2,'a') as infile2:
+            for vname in invars:
+                print(vname)
+                indata1 = infile1.variables[vname][:]
+                indata2 = infile2.variables[vname][:]
+                infile1.variables[vname][:] = (indata2[:]-indata1[:])*(iday-istart)/(iend-istart)+indata1[:]
